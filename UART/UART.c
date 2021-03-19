296b560baae98a1a77c4119f53a8870a7115eb4b
M4rc311o <71455092+M4rc311o@users.noreply.github.com>
2021-03-19 20:13:27 +0100

    Update UART.c

--- UART/UART.c
+++ UART/UART.c
@@ -1,15 +1,15 @@
 /*
- * Serial.c
+ * UART.c
  *
  * Created: 13.03.2021 16:39:31
  * Author : M4rc3110
  */ 
 
-#include "USART.h"
+#include "UART.h"
 
 FILE str_uart = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
 
-void USART_Init(uint32_t BAUD_RATE){
+void UART_Init(uint32_t BAUD_RATE){
 	uint32_t UBRR = (F_CPU/(16*BAUD_RATE))-1;
 	
 	//set baud rate
