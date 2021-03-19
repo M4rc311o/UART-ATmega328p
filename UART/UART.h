79c2a9b306e321b819e4a52ebafe09af03e3e5da
M4rc311o <71455092+M4rc311o@users.noreply.github.com>
2021-03-19 20:14:19 +0100

    Update UART.h

--- UART/UART.h
+++ UART/UART.h
@@ -1,18 +1,18 @@
 /*
- * Serial.h
+ * UART.h
  *
  * Created: 13.03.2021 16:40:00
  * Author : M4rc3110
  */ 
 
 
-#ifndef USART_H_
-#define USART_H_
+#ifndef UART_H_
+#define UART_H_
 
 #include <avr/io.h>
 #include <stdio.h>
 
-void USART_Init(uint32_t BAUD_RATE);
+void UART_Init(uint32_t BAUD_RATE);
 void uart_putchar(char c, FILE *stream);
 
-#endif /* USART_H_ */
+#endif /* UART_H_ */
